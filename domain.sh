3722926d6f86175aa8cde4475b0f59c91aea78f3
risinghunt3r <53638987+risinghunt3r@users.noreply.github.com>
2020-03-14 13:16:48 +0530

    Update domain.sh

--- domain.sh
+++ domain.sh
@@ -19,6 +19,11 @@ amass enum -d $1 >>$1.txt
 res=$(cat $1.txt|wc -l)
 echo "[*] Results: " $res
 
+echo "[+] crtsh"
+curl -s https://crt.sh/?q=%.$1  | sed 's/<\/\?[^>]\+>//g'|sed 's~^    ~~'|grep $1|tail -n +3 >>$1.txt
+res=$(wc -l $1.txt)
+echo "[*] Results: " $res
+
 echo "[+] unique_subdomains"
 cat $1.txt|sort -u >allsubdomains.txt
 res=$(cat allsubdomains.txt|wc -l)
